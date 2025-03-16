eca8a9b7412b7f853797660dfd76384160e8d050
ykertytsky <y.kertytsky@gmail.com>
2025-03-16 12:48:13 +0200

    simplistic js tictactoe project

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,110 @@
-import Image from "next/image";
+'use client';
+
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [isWon, setIsWon] = useState(null)
+  
+  const [figureToPlace, setFigureToPlace] = useState(
+    useEffect(() => {
+      const randnum = Math.random();
+      
+      if (randnum > 0.5) {
+        setFigureToPlace("X");
+      } else {
+        setFigureToPlace("0");
+      }
+    }, [])
+  );
+  
+  const winningCombinations = [
+    [1, 1, 1, 2, 2, 2, 2, 2, 2], // Top row
+    [2, 2, 2, 1, 1, 1, 2, 2, 2], // Middle row
+    [2, 2, 2, 2, 2, 2, 1, 1, 1], // Bottom row
+    [1, 2, 2, 1, 2, 2, 1, 2, 2], // Left column
+    [2, 1, 2, 2, 1, 2, 2, 1, 2], // Middle column
+    [2, 2, 1, 2, 2, 1, 2, 2, 1], // Right column
+    [1, 2, 2, 2, 1, 2, 2, 2, 1], // Main diagonal
+    [2, 2, 1, 2, 1, 2, 1, 2, 2], // Anti-diagonal
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const checkWinningCombination = (element, board) => {
+    const boardCopy = [...board];
+    for (let i = 0; i<9; i++) {
+      if (board[i] === element) {
+        boardCopy[i] = 1;
+      } else {
+        boardCopy[i] = 2;
+      };
+    }
+
+  return winningCombinations.some((winCombo) =>
+    winCombo.every((val, index) => val === boardCopy[index])
+  )};
+
+
+  const [cells, setCells] = useState([
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+    "_",
+  ]);
+
+  const swapElement = (currentElement) => {
+    if (currentElement == "X") {
+      return "0"
+    } else {
+      return "X"
+    }
+  }
+
+
+  const onClickHandler = (cellIndex) => {
+    const updateCells = cells.map((element, elementIndex) => {
+      if (elementIndex === cellIndex && element === "_") {
+        setFigureToPlace(swapElement(figureToPlace))
+        return figureToPlace;
+      };
+      return element;
+    });
+    setCells(updateCells)
+    if (checkWinningCombination(figureToPlace, updateCells) == true) {
+      setIsWon(true)
+    } else {
+      setIsWon(false)
+    }
+  };
+
+  return (
+    <div className="flex flex-col min-h-screen justify-center items-center gap-5">
+      <h1 className="font-bold text-4xl">
+          {isWon ? `${swapElement(figureToPlace)} won!` : `${figureToPlace}'s Turn`}
+      </h1>
+      {isWon ? null : (
+         <div className="game-grid">
+        <div className="game-row flex flex-row space-x-2 text-8xl">
+          <div onClick={() => onClickHandler(0)}>{cells[0]}</div>
+          <div onClick={() => onClickHandler(1)}>{cells[1]}</div>
+          <div onClick={() => onClickHandler(2)}>{cells[2]}</div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="game-row flex flex-row space-x-2 text-8xl">
+          <div onClick={() => onClickHandler(3)}>{cells[3]}</div>
+          <div onClick={() => onClickHandler(4)}>{cells[4]}</div>
+          <div onClick={() => onClickHandler(5)}>{cells[5]}</div>
+        </div>
+        <div className="game-row flex flex-row space-x-2 text-8xl">
+          <div onClick={() => onClickHandler(6)}>{cells[6]}</div>
+          <div onClick={() => onClickHandler(7)}>{cells[7]}</div>
+          <div onClick={() => onClickHandler(8)}>{cells[8]}</div>
+        </div>
+      </div>
+      )}
+     
     </div>
   );
-}
+} 
